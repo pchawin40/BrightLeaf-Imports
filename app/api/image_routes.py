d8609987b8cca5828e64d498bde15cf3d12c3f4d
Chawin Pathompornvivat <pathocha000@gmail.com>
2022-11-10 16:58:06 -0800

    Successfully completed rough draft of review routes | 11.10.22 4:21 PM - CP

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -8,59 +8,80 @@ from .auth_routes import validation_errors_to_error_messages
 image_routes = Blueprint('images', __name__)
 
 #* GET /images
-@image_routes.route('/', methods=['GET', 'POST'])
+@image_routes.route('/')
 def images():
   """
   GET: Get all availables images
-  POST: Create new image
   """
-  # request method is post, create new image
-  if request.method == 'POST':
-    form = ImageForm()
-    
-    # Get the csrf_token from the request cookie and put it into the
-    # form manually to validate_on_submit can be used
-    form['csrf_token'].data = request.cookies['csrf_token']
-    
-    # check if pass on submit validation
-    if form.validate_on_submit():
-      # create new image
-      new_image = Image(
-        imageable_id=form.data['imageable_id'],
-        imageable_type=form.data['imageable_type'],
-        url=form.data['url'],
-        description=form.data['description']
-      )
-
-      # add and commit change
-      db.session.add(new_image)
-      db.session.commit()
-    
-      # return successful response
-      return new_image.to_dict()
-    
-    # else if form did not pass validation, return error
-    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
-  
   # otherwise, method is get
   images = Image.query.all()
   return {'images': {image.id: image.to_dict() for image in images}}
 
+#* POST /images
+@image_routes.route('/', methods=['POST'])
+@login_required
+def secure_images():
+  """
+    POST: Create a new image
+  """
+  # request method is post, create new image
+  form = ImageForm()
+    
+  # Get the csrf_token from the request cookie and put it into the
+  # form manually to validate_on_submit can be used
+  form['csrf_token'].data = request.cookies['csrf_token']
+  
+  # check if pass on submit validation
+  if form.validate_on_submit():
+    # create new image
+    new_image = Image(
+      imageable_id=form.data['imageable_id'],
+      imageable_type=form.data['imageable_type'],
+      url=form.data['url'],
+      description=form.data['description']
+    )
+
+    # add and commit change
+    db.session.add(new_image)
+    db.session.commit()
+
+    # return successful response
+    return new_image.to_dict()
+    
+  # else if form did not pass validation, return error
+  return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
 #* GET /images/:imageId
-#* PUT /images/:imageId
-#* DELETE /images/:imageId
-@image_routes.route('/<int:image_id>', methods=['GET', 'PUT', 'DELETE'])
+@image_routes.route('/<int:image_id>')
 def image_by_id(image_id):
   """
   GET: Get image by id
-  PUT: Update image by id
   """
+  # get image by image_id
   image = Image.query.get(image_id)
   
   # check if image is found
   if image == None:
     return {'errors': [f"Image {image_id} does not exist"]}, 404
   
+  return image.to_dict()
+
+#* PUT /images/:imageId
+#* DELETE /images/:imageId
+@image_routes.route('/<int:image_id>', methods=['PUT', 'DELETE'])
+@login_required
+def secure_image_by_id(image_id):
+  """
+  PUT: Update image by id
+  DELETE: Delete image by id
+  """
+  # get image by image_id
+  image = Image.query.get(image_id)
+    
+  # check if image is found
+  if image == None:
+    return {'errors': [f"Image {image_id} does not exist"]}, 404
+
   # [PUT] 
   if request.method == 'PUT':
           
@@ -98,6 +119,5 @@ def image_by_id(image_id):
     db.session.commit()
     
     # return successful response with delete image message
-    return {'image': f"Successfully deleted image {image.id}"}
+    return {'message': f"Successfully deleted image {image.id}"}
   
-  return image.to_dict()
