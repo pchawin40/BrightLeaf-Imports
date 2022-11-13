b117c59104794b509b53b4c2193f441deb9e43f8
Chawin Pathompornvivat <pathocha000@gmail.com>
2022-11-13 11:30:53 -0800

    Rough draft top landing | 11:30 AM 11.13.22 - CP

--- react-app/src/components/LandingPage/TopLanding/TopLanding.js
+++ react-app/src/components/LandingPage/TopLanding/TopLanding.js
@@ -8,7 +8,6 @@ const TopLanding = () => {
   return (
     <section id="top-landing-section">
       {/* Image background */}
-
       <figure
         id="background-img-container"
         style={{
@@ -16,8 +15,15 @@ const TopLanding = () => {
           backgroundImage: 'url("https://static.wixstatic.com/media/55ccf4_aa180e707df1406f94b0caf7b8468419~mv2.jpg/v1/fit/w_1489,h_821,q_90/55ccf4_aa180e707df1406f94b0caf7b8468419~mv2.jpg")'
         }}
       >
+
         {/* Title */}
         <section>
+          {/* Back Figure */}
+          <figure id="tls-back-figure">
+            <i className="fa-solid fa-chevron-left fa-xl" />
+            <span>Back</span>
+          </figure>
+
           <figure
             id="tls-brightleaf-logo-container"
           >
@@ -34,22 +40,27 @@ const TopLanding = () => {
 
         {/* Image Carousel */}
         <aside>
-          <figure className="tls-ic-figure">
+          <figure className="trs-ic-figure">
             {/* left clicker */}
-            <figure className="tls-ic-inner-figure left-clicker">
+            <figure className="trs-ic-inner-figure left-clicker">
               <i className="fa-solid fa-chevron-left fa-2xl" />
             </figure>
 
             <img
-              className="tls-ic-img"
+              className="trs-ic-img"
               src="https://res.cloudinary.com/dfz7bzhoi/image/upload/v1668326915/image-gallery_dyqr4i.webp"
-              alt="tl-carousel"
+              alt="trs-carousel"
             />
 
             {/* right clicker */}
-            <figure className="tls-ic-inner-figure right-clicker">
+            <figure className="trs-ic-inner-figure right-clicker">
               <i className="fa-solid fa-chevron-right fa-2xl" />
             </figure>
+          </figure>
+
+          <figure id="trs-scroll-figure">
+            <span>Scroll</span>
+            <span className="line-span trs-scroll" />
           </figure>
         </aside>
       </figure>
