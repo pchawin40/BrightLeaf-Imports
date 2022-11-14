260f33b7a6248f3e458e9778fef2fd00036b2cad
Chawin Pathompornvivat <pathocha000@gmail.com>
2022-11-13 20:39:51 -0800

    Updating styling top landing page | 8:39 PM 11.13.22 - CP

---
+++ react-app/src/components/LandingPage/TopLanding/BackModal/BackModal.js
@@ -0,0 +1,13 @@
+// src/components/LandingPage/TopLanding/BackModal/BackModal.js
+
+//? BackModal component
+const BackModal = () => {
+  return (
+    <section id="back-modal-section">
+      
+    </section>
+  );
+};
+
+// export default component
+export default BackModal;
