4943d34a14100c8c15caf13dde55300925ea7c90
Chawin Pathompornvivat <pathocha000@gmail.com>
2022-11-14 11:31:28 -0800

    Finished rough draft lower top landing page | 11:31 AM 11.14.22 - CP

--- react-app/src/components/LandingPage/LowerLanding/LLLowerTop/LLLowerTop.js
+++ react-app/src/components/LandingPage/LowerLanding/LLLowerTop/LLLowerTop.js
@@ -6,8 +6,80 @@ import './LLLowerTop.css';
 //? LLLowerTop component
 const LLLowerTop = () => {
   return (
+    // Lower Top
     <section id="ll-lts">
-      
+      {/* Lower Top: Left */}
+      <section id="lts-left">
+        {/* Lower Top: Left: Top */}
+        <section id="lts-left-top">
+          {/* Lower Top: Left: Top: Inner */}
+          <figure id="lts-left-top-inner-figure">
+            {/* Picture */}
+            <img
+              src="https://res.cloudinary.com/dfz7bzhoi/image/upload/v1668450314/Brightleaf%20Imports/102720045_614676142740747_20774271708246_nz7c6v.webp"
+              alt="living-room-display"
+            />
+          </figure>
+        </section>
+
+        {/* Lower Top: Left: Lower */}
+        <section id="lts-left-lower">
+          {/* Our Pieces */}
+          <figure>
+            <i className="fa-sharp fa-solid fa-square fa-xs" />
+            <span>
+              Our Pieces
+            </span>
+          </figure>
+          {/* Text */}
+          <ul>
+            <li>
+              Coffee
+            </li>
+            <li>
+              Dining
+            </li>
+            <li>
+              Conferences
+            </li>
+            <li>
+              End Tables
+            </li>
+          </ul>
+          {/* Button */}
+          <button>
+            Shop
+          </button>
+        </section>
+
+      </section>
+
+      {/* Lower Top: Right */}
+      <section id="lts-right">
+        {/* Lower Top: Right: Top */}
+        <section id="lts-right-top">
+          {/* Inner */}
+          <section id="lts-right-top-inner">
+            {/* Text */}
+            <h2>
+              Carving out
+              <br />
+              excellence.
+            </h2>
+          </section>
+        </section>
+
+        {/* Lower Top: Right: Lower */}
+        <section id="lts-right-lower">
+          {/* Picture */}
+          <figure id="lts-right-lower-figure">
+            <img
+              src="https://res.cloudinary.com/dfz7bzhoi/image/upload/v1668453546/Brightleaf%20Imports/55ccf4_5c13e82c4bc94d2eb3e0e99db87dcb16f000_eb4nrs.webp"
+              alt="craft-tables"
+            />
+          </figure>
+        </section>
+      </section>
     </section>
   );
 };
