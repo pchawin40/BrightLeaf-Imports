73d08e9f7aa0de8dedc2c1c67b539198e54de383
Chawin Pathompornvivat <pathocha000@gmail.com>
2022-11-29 22:31:45 -0800

    Update on contact map | 10:30 PM 11.29.22 - CP

---
+++ app/api/map_routes.py
@@ -0,0 +1,21 @@
+import os
+from flask import Blueprint
+
+map_routes = Blueprint('maps', __name__)
+
+#* GET: /api/maps/key
+@map_routes.route('/key', methods=['POST'])
+def post_map_key():
+  """
+  Post Map Key: sends public key to backend to encrypt API key to prevent stealing of data
+  """
+  
+  print()
+  print()
+  print("os environ", os.environ)
+  print()
+  print()
+  
+  return {"googleMapsAPIKey": os.environ.get('MAPS_API_KEY')}
+  
+  
