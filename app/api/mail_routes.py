24d1780226600eb48ef0e286cc8b3653cd705557
Chawin Pathompornvivat <pathocha000@gmail.com>
2022-12-03 13:24:51 -0800

    Modified email sending | 1:24 PM 12.3.22 - CP

--- app/api/mail_routes.py
+++ app/api/mail_routes.py
@@ -1,10 +1,10 @@
 import os
 import smtplib 
 
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 from flask import Flask 
-from flask_mail import Mail
 from app.forms import EmailForm
+from .auth_routes import validation_errors_to_error_messages
 
 mail_routes = Blueprint('mail', __name__)
 
@@ -14,15 +14,22 @@ def post_subscribe_mail():
     """
     method for using w/ flask_mail and for resetting user's password
     """
-    form = EmailForm()
     
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:  
+      form = EmailForm()
+      
+      # validate csrf token
+      form['csrf_token'].data = request.cookies['csrf_token']
+      
+      if form.validate_on_submit():
         email_address = os.environ.get('EMAIL_USER')
         email_password = os.environ.get('EMAIL_PASSWORD')
         connection.login(email_address, email_password )
         connection.sendmail(from_addr=email_address, to_addrs=form.data['email'], 
         msg="subject:Brightleaf Imports Subscription \n\n Hello, you have subscribed to Brightleaf Imports LLC. We'll send you update on our product and any news we may have.")
-    
+
         return "Success"
-    return "Failed"
+      return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+    return {'errors': [f"Email was not sent successfully"]}, 404
+      
   
